309b3ce76756bbeba795d78b34014a66a819368a
yecheng.123 <yecheng.123@bytedance.com>
2022-09-14 23:40:19 +0800

    info

--- change_setting.sh
+++ change_setting.sh
@@ -16,8 +16,9 @@ jar uf app.jar BOOT-INF/classes/application-auto.properties
 ENTRYPOINT [\"java\", \"-jar\", \"app.jar\"] " >> Dockerfile
 
 #开始构建镜像
-echo "begin build img, image name is:$1/$2_$3"
-docker build -t $1/$2_$3 .
+imageName=$(echo "$1/$2_$3" | tr 'A-Z' 'a-z') 
+echo "begin build img, image name is:${imageName}"
+docker build -t ${imageName} .
 
 #清理工作
 cd ..
