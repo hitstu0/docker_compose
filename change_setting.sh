851141ac33f5b3d4f65b9f67519985cc2450bae6
yecheng.123 <yecheng.123@bytedance.com>
2022-09-15 11:18:34 +0800

    info

--- change_setting.sh
+++ change_setting.sh
@@ -9,13 +9,12 @@ cd ${fileName}
 echo "make file success, file is $(pwd)"
 
 BaseImageName=$(echo "$1/$2" | tr 'A-Z' 'a-z') 
-#改变jar包配置
+#添加配置文件
 echo "FROM ${BaseImageName}
-RUN mkdir -p BOOT-INF/classes \\
-&& echo \"server.port=$3 \\n spring.cloud.consul.discovery.instance-id=$1_$2_$3 \\n spring.cloud.consul.host=120.77.221.92 \\n spring.cloud.consul.port=8500 \" >> BOOT-INF/classes/application-auto.properties \\ 
-&& jar uf app.jar BOOT-INF/classes/application-auto.properties \\
-&& rm -rf BOOT-INF" >> Dockerfile
-#ENTRYPOINT [\"java\", \"-jar\", \"app.jar\"]
+RUN mkdir config \\
+&& echo \"server.port=$3 \\n spring.cloud.consul.discovery.instance-id=$1_$2_$3 \\n spring.cloud.consul.host=120.77.221.92 \\n spring.cloud.consul.port=8500 \" >> config/application-auto.properties \\ 
+ENTRYPOINT [\"java\", \"-jar\", \"app.jar\"] " >> Dockerfile
+
 #开始构建镜像
 imageName=$(echo "$1/$2_$3" | tr 'A-Z' 'a-z') 
 echo "begin build img, image name is:${imageName}"
