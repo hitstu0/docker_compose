a247207667ad39b1e4227e37f4c1f4054138c52d
yecheng.123 <yecheng.123@bytedance.com>
2022-09-17 15:55:29 +0800

    info

--- image_generator.sh
+++ image_generator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "begin generate image, depository is: $1, file is: $2,  port is: $3";
+echo "begin generate image, depository is: $1, file is: $2";
 
 #基础镜像名格式：'仓库/文件夹', 该镜像包含不含有配置文件的jar包
 BaseImageName=$(echo "$1/$2" | tr 'A-Z' 'a-z')
@@ -9,9 +9,9 @@ echo "base image name is: ${BaseImageName}"
 CON=`docker image ls "${BaseImageName}" | wc -l`
 echo "count is ${CON}"
 
-if [ $CON == 2 ]
+if [ $CON != 2 ]
 then
-   #如果存在则以该镜像为基础修改配置 
+   #如果不存在则生基础镜像
    echo "base image not exist, begin generate image"
    source generate_base.sh $1 $2 
 fi
