2b3511adfc15d145bae4c940a6bf7fb3c20e5142
yecheng.123 <yecheng.123@bytedance.com>
2022-09-24 15:47:15 +0800

    info

--- adjust_nums.sh
+++ adjust_nums.sh
@@ -36,8 +36,8 @@ then
 
        #启动新容器
 
-       containerId=$(docker run -d -p ${port}:${port} --network=nginx --name="p${port}" ${image_name}\
-       java -jar app.jar --server.port=${port} --spring.cloud.consul.discovery.instance-id=${image_name}_${port})
+       containerId=$(docker run -d -p ${port}:${port} --network=nginx --name="${image_name}test${port}" ${image_name}\
+       java -jar app.jar --server.port=${port} --spring.cloud.consul.discovery.instance-id=${image_name})
        echo "container start success, id is ${containerId}"
 
        old_nums=$((old_nums + 1))
@@ -63,28 +63,28 @@ else
 fi
 
 #删除旧nginx配置
-rm -rf nginx/tlocation/${image_name}.conf
-rm -rf nginx/tupstream/${image_name}.conf
+#rm -rf nginx/tlocation/${image_name}.conf
+#rm -rf nginx/tupstream/${image_name}.conf
 
-if [ $new_nums != 0 ]
-then
+#if [ $new_nums != 0 ]
+#then
    #生成upstream
-   servers=$(docker ps --format "{{.Image}} {{.Names}}" | grep ${image_name} | awk '{print"server "$2":"substr($2,2)";"}')
-   upstream="upstream ${image_name} {
-      ${servers}
-   }"
-   echo "${upstream}" >> nginx/tupstream/${image_name}.conf
+ #  servers=$(docker ps --format "{{.Image}} {{.Names}}" | grep ${image_name} | awk '{print"server "$2":"substr($2,2)";"}')
+  #upstream="upstream ${image_name} {
+   #   ${servers}
+   #}"
+   #echo "${upstream}" >> nginx/tupstream/${image_name}.conf
 
    #生成localtion代码
-   location="location /${image_name} {
-      proxy_pass http://${image_name};
-   }"
-   echo "${location}" >> nginx/tlocation/${image_name}.conf
+   #location="location /${image_name} {
+     # proxy_pass http://${image_name};
+  # }"
+   #echo "${location}" >> nginx/tlocation/${image_name}.conf
 
    #重启nginx
-   docker stop nginx
-   cd nginx
-   docker-compose up
-fi
+   #docker stop nginx
+   #cd nginx
+   #docker-compose up
+#fi
 
 
