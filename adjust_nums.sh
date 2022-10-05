896785e777a7197e89504b8d1f60d637dadf76c7
yecheng.123 <yecheng.123@bytedance.com>
2022-10-05 10:36:19 +0800

    info

--- adjust_nums.sh
+++ adjust_nums.sh
@@ -62,29 +62,3 @@ else
    echo "number not change, not need to adjust"
 fi
 
-#删除旧nginx配置
-#rm -rf nginx/tlocation/${image_name}.conf
-#rm -rf nginx/tupstream/${image_name}.conf
-
-#if [ $new_nums != 0 ]
-#then
-   #生成upstream
- #  servers=$(docker ps --format "{{.Image}} {{.Names}}" | grep ${image_name} | awk '{print"server "$2":"substr($2,2)";"}')
-  #upstream="upstream ${image_name} {
-   #   ${servers}
-   #}"
-   #echo "${upstream}" >> nginx/tupstream/${image_name}.conf
-
-   #生成localtion代码
-   #location="location /${image_name} {
-     # proxy_pass http://${image_name};
-  # }"
-   #echo "${location}" >> nginx/tlocation/${image_name}.conf
-
-   #重启nginx
-   #docker stop nginx
-   #cd nginx
-   #docker-compose up
-#fi
-
-
