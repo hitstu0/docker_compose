1437263058a42a89c0ccf752178118c12a8c4db0
yecheng.123 <yecheng.123@bytedance.com>
2022-10-25 22:16:09 +0800

    info

---
+++ api/ApiStarter.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+image_name=api_manager
+theIP=120.78.165.96
+
+       #查询可用端口
+       port=0
+       while [ $port -eq 0 ]
+       do
+           ran=$(($RANDOM + 100000))
+           result=$((ran%64511 + 1024))
+           echo "random port is: ${result}"
+
+           portNums=$(netstat -anp | grep $result | wc -l)
+           if [ $portNums -eq 0 ] 
+           then
+               port=$result
+           fi
+       done
+       echo "get available port $port"
+
+       #启动新容器
+
+       containerId=$(docker run -d -cap-add SYS_PTRACE -p ${port}:${port} -v /root/logs/${image_name}/${port}:/logs  --name="${image_name}_${port}" ${image_name}\
+       java -jar app.jar --service_name=$1 --spring.cloud.consul.host=${theIP} --spring.cloud.consul.port=8500 --server.port=${port} --spring.cloud.consul.discovery.instance-id=${image_name})
+       echo "container start success, id is ${containerId}"
---
+++ serviceStarter.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+echo "begin adjust container:$1 apiPort is : $2"
+image_name=$1
+
+theIP=120.78.165.96
+
+new_nums=$2
+if [ $new_nums -lt 0 ]
+then
+   new_nums=0
+fi
+
+#查询可用端口
+port=0
+while [ $port -eq 0 ]
+do
+    ran=$(($RANDOM + 100000))
+    result=$((ran%64511 + 1024))
+    echo "random port is: ${result}"
+
+    portNums=$(netstat -anp | grep $result | wc -l)
+    if [ $portNums -eq 0 ] 
+    then
+       port=$result
+    fi
+done
+echo "get available port $port"
+
+#启动新容器
+
+javaopt=-XX:CITime
+
+containerId=$(docker run -d -l SERVICE_tags=apihost=${theIP},apiport=$2,weight=1 -cap-add SYS_PTRACE -p ${port}:${port} -v /root/logs/${image_name}/${port}:/logs  --name="${image_name}_${port}" ${image_name} \
+java ${javaopt} -jar app.jar --spring.cloud.consul.host=${theIP} --spring.cloud.consul.port=8500 --server.port=${port} --spring.cloud.consul.discovery.instance-id=${image_name})
+echo "container start success, id is ${containerId}"
+
+
