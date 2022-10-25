4443b6ffd5ccd09523fb46730848bfa4cfc146ce
yecheng.123 <yecheng.123@bytedance.com>
2022-10-25 20:04:06 +0800

    info

--- adjust_nums.sh
+++ adjust_nums.sh
@@ -8,16 +8,6 @@ then
    new_nums=0
 fi
 
-#查询当前容器数
-old_nums=$(docker ps --format {{.Image}} | grep ${image_name} | wc -l)
-echo "now number is: ${old_nums}"
-
-if [ $new_nums -gt $old_nums ]
-then
-   echo "begin to expansion"
-
-   while [ $old_nums -lt $new_nums ]
-   do
        #查询可用端口
        port=0
        while [ $port -eq 0 ]
@@ -36,29 +26,8 @@ then
 
        #启动新容器
 
-       containerId=$(docker run -d -l SERVICE_tags=apihost=$3,apiport=${port},weight=3 -p ${port}:${port} -v /root/logs/${image_name}/${port}:/logs  --name="${image_name}_${port}" ${image_name}\
+       containerId=$(docker run -d -l SERVICE_tags=apihost=$3,apiport=$4,weight=1 -p ${port}:${port} -v /root/logs/${image_name}/${port}:/logs  --name="${image_name}_${port}" ${image_name}\
        java -jar app.jar --logging.path=./logs --spring.cloud.consul.host=$3 --spring.cloud.consul.port=8500 --server.port=${port} --spring.cloud.consul.discovery.instance-id=${image_name})
        echo "container start success, id is ${containerId}"
 
-       old_nums=$((old_nums + 1))
-   done
-
-elif [ $new_nums -lt $old_nums ]
-then
-   echo "begin to shrink"
-
-   while [ $old_nums -gt $new_nums ] 
-   do
-      #查询容器id
-      deleId=$(docker ps | grep $1 | awk '{print$1}' | sed -n '1p')
-
-      #停止并删除容器
-      docker stop $deleId
-      docker rm $deleId
-      old_nums=$((old_nums - 1))
-   done
- 
-else 
-   echo "number not change, not need to adjust"
-fi
 
