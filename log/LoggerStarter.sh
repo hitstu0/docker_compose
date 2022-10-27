2c835b83e9b5acd9ddd2378fef55735cddd7492e
yecheng.123 <yecheng.123@bytedance.com>
2022-10-27 11:50:56 +0800

    info

--- log/LoggerStarter.sh
+++ log/LoggerStarter.sh
@@ -20,7 +20,7 @@ theIP="120.78.165.96"
        echo "get available port $port"
 
        #启动新容器
-       javaopt="-Xms=50m -Xmx=200m"
+       javaopt="-Xms=50m -Xmx=128m -Xmn=64m"
 
        containerId=$(docker run -d -p ${port}:${port} -v /root/logs:/root/logs  --name="${image_name}_${port}" ${image_name}\
        java -jar ${javaopt} app.jar  --logging.path=./logs --spring.cloud.consul.host=${theIP} --spring.cloud.consul.port=8500 --server.port=${port} --spring.cloud.consul.discovery.instance-id=${image_name})
