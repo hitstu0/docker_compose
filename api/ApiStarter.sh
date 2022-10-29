0a426b0eeaa6b10c5cf79d57487879b78ef88513
yecheng.123 <yecheng.123@bytedance.com>
2022-10-29 16:46:29 +0800

    info

--- api/ApiStarter.sh
+++ api/ApiStarter.sh
@@ -20,6 +20,6 @@ theIP=120.78.165.96
 
        #启动新容器
 
-       containerId=$(docker run -d -cap-add SYS_PTRACE -p ${port}:${port} -v /root/logs/${image_name}/${port}:/logs  --name="${image_name}_${port}" ${image_name}\
+       containerId=$(docker run -d  -p ${port}:${port} -v /root/logs/${image_name}/${port}:/logs  --name="${image_name}_${port}" ${image_name}\
        java -jar -Xms32m -Xmx128m -Xss32m -XX:MaxDirectMemorySize=32m -XX:MaxMetaspaceSize=32m app.jar --service_name=$1 --spring.cloud.consul.host=${theIP} --spring.cloud.consul.port=8500 --server.port=${port} --spring.cloud.consul.discovery.instance-id=${image_name})
        echo "container start success, id is ${containerId}"
