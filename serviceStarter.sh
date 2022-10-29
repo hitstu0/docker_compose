4a84785b7100c4037fff15ddbf83658ed7e3c53c
yecheng.123 <yecheng.123@bytedance.com>
2022-10-29 15:01:34 +0800

    info

--- serviceStarter.sh
+++ serviceStarter.sh
@@ -28,7 +28,7 @@ echo "get available port $port"
 
 #启动新容器
 
-javaopt="-Xms=50m -Xmx=200m"
+javaopt="-Xms50m -Xmx200m"
 
 containerId=$(docker run -d --init -l SERVICE_tags=apihost=${theIP},apiport=$2,weight=1 --cap-add=SYS_PTRACE -p ${port}:${port} -p 7000:7000 -v /root/logs/${image_name}/${port}:/logs  --name="${image_name}_${port}" ${image_name} \
 java -jar ${javaopt} app.jar --spring.cloud.consul.host=${theIP} --spring.cloud.consul.port=8500 --server.port=${port} --spring.cloud.consul.discovery.instance-id=${image_name})
