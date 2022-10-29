8965273bc768024b67784171552bbc0cc377be55
yecheng.123 <yecheng.123@bytedance.com>
2022-10-29 21:02:26 +0800

    info

--- start_service.sh
+++ start_service.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "begin generater api gateway for:$1,host is; $2"
-
+echo "begin generater api gateway for:$1"
+IP="120.78.165.96"
 #启动API网关
 port=0
 while [ $port -eq 0 ]
@@ -17,10 +17,10 @@ do
 done
 echo "get available port $port"
 
-docker run -d -l -p ${port}:${port} -v /root/logs/api/${port}:/log --name="api_$1" api_gateway\
-java -jar app.jar --spring.cloud.consul.host=$2 --spring.cloud.consul.port=8500 --server.port=${port} --spring.cloud.consul.discovery.instance-id="api_$1"
+docker run -d -p ${port}:${port} -v /root/logs/api/${port}:/log --name="api_$1" api_gateway \
+java -jar app.jar --spring.cloud.consul.host=${IP} --spring.cloud.consul.port=8500 --server.port=${port} --spring.cloud.consul.discovery.instance-id="api_$1"
 
-curl $2:$port/refresh
+curl ${IP}:$port/refresh
 
 mport=0
 while [ $mport -eq 0 ]
@@ -37,6 +37,6 @@ do
 done
 echo "get available port $mport"
 
-containerId=$(docker run -d -l SERVICE_tags=apihost=$2,apiport=${port},weight=3 -p ${mport}:${mport} -v /root/logs/$1/${mport}:/log  --name="$1_${mport}" $1\
-java -jar app.jar --spring.cloud.consul.host=$2 --spring.cloud.consul.port=8500 --server.port=${mport} --spring.cloud.consul.discovery.instance-id=$1)
+containerId=$(docker run -d -l SERVICE_tags=apihost=${IP},apiport=${port},weight=3 -p ${mport}:${mport} -v /root/logs/$1/${mport}:/log  --name="$1_${mport}" $1 \
+java -jar app.jar --spring.cloud.consul.host=${IP} --spring.cloud.consul.port=8500 --server.port=${mport} --spring.cloud.consul.discovery.instance-id=$1)
 echo "container start success, id is ${containerId}"
