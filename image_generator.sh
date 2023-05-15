1a8e79b3da38d33d97abb24e8518b3c3e7c756f8
yecheng <2064355949@qq.com>
2023-05-15 10:59:43 +0800

    info

--- image_generator.sh
+++ image_generator.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 echo "begin generate image, depository is: $1, file is: $2";
 
-#基础镜像名格式：'仓库/文件夹', 该镜像包含不含有配置文件的jar包
-BaseImageName=$(echo "$1/$2" | tr 'A-Z' 'a-z')
-echo "base image name is: ${BaseImageName}"
-
-#判断是否有该基础镜像
-CON=$(docker image ls "${BaseImageName}" | wc -l)
-echo "count is ${CON}"
-
-if [ $CON != 2 ]
-then
-   #如果不存在则生基础镜像
-   echo "begin generate base image, depository is: $1, file is: $2";
-
 #根据仓库名生成git url
 gitPre="https://github.com/hitstu0";
 gitUrl="${gitPre}/${1}.git"
@@ -50,7 +37,6 @@ cd ../..
 rm -rf "gitcode_$1_$2"
 
 cd $3/docker_compose
-fi
 
 
 
