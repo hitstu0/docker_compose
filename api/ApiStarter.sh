04ce78ebad3001a74242468c7bfdeede377cb35b
hitstu0 <2064355949@qq.com>
2023-05-22 11:19:04 +0800

    info

--- api/ApiStarter.sh
+++ api/ApiStarter.sh
@@ -20,6 +20,6 @@ theIP=$2
        export API_PORT=$port
        #启动新容器
 
-       containerId=$(docker run -d  -p ${port}:${port} -v /root/logs/${image_name}/${port}:/logs  --name="${image_name}_${port}" ${image_name}\
+       containerId=$(docker run -d  -p ${port}:${port} -v /root/logs/${image_name}/${port}:/log  --name="${image_name}_${port}" ${image_name}\
        java -jar app.jar --service_name=$1 --spring.cloud.consul.host=${theIP} --spring.cloud.consul.port=8500 --server.port=${port} --spring.cloud.consul.discovery.instance-id=${image_name})
        echo "container start success, id is ${containerId}"
